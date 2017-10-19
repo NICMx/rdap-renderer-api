398c839e91902ac7bf2878178983bf23d85d3dc9
dhfelix <daniel.hdz.felix@hotmail.com>
2017-10-19 16:22:30 -0500

    Add JavaDoc comments

--- rdap-renderer-api/src/main/java/mx/nic/rdap/renderer/Renderer.java
+++ rdap-renderer-api/src/main/java/mx/nic/rdap/renderer/Renderer.java
@@ -18,24 +18,124 @@ import mx.nic.rdap.renderer.object.SearchResponse;
  */
 public interface Renderer {
 
+	/**
+	 * Render an {@link Entity} response to the specified {@link PrintWriter}
+	 * <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderEntity(RequestResponse<Entity> response, PrintWriter printWriter);
 
+	/**
+	 * Render a {@link Domain} response to the specified {@link PrintWriter}
+	 * <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderDomain(RequestResponse<Domain> response, PrintWriter printWriter);
 
+	/**
+	 * Render an {@link Autnum} response to the specified {@link PrintWriter}
+	 * <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderAutnum(RequestResponse<Autnum> response, PrintWriter printWriter);
 
+	/**
+	 * Render a {@link Nameserver} response to the specified {@link PrintWriter}
+	 * <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderNameserver(RequestResponse<Nameserver> response, PrintWriter printWriter);
 
+	/**
+	 * Render an {@link IpNetwork} response to the specified {@link PrintWriter}
+	 * <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderIpNetwork(RequestResponse<IpNetwork> response, PrintWriter printWriter);
 
+	/**
+	 * Render an {@link Entity} {@link SearchResponse} to the specified
+	 * {@link PrintWriter} <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderEntities(SearchResponse<Entity> response, PrintWriter printWriter);
 
+	/**
+	 * Render a {@link Domain} {@link SearchResponse} to the specified
+	 * {@link PrintWriter} <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderNameservers(SearchResponse<Nameserver> response, PrintWriter printWriter);
 
+	/**
+	 * Render a {@link Nameserver} {@link SearchResponse} to the specified
+	 * {@link PrintWriter} <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderDomains(SearchResponse<Domain> response, PrintWriter printWriter);
 
+	/**
+	 * Render an {@link ExceptionResponse} to the specified {@link PrintWriter}
+	 * <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderException(ExceptionResponse response, PrintWriter printWriter);
 
+	/**
+	 * Render {@link HelpResponse} to the specified {@link PrintWriter}
+	 * <code>printWriter</code>
+	 * 
+	 * @param response
+	 *            Response that contains the object to render in the
+	 *            <code>PrintWriter</code>
+	 * @param printWriter
+	 *            <code>PrintWriter</code> to use for output
+	 */
 	public void renderHelp(HelpResponse response, PrintWriter printWriter);
 
 }
--- rdap-renderer-api/src/main/java/mx/nic/rdap/renderer/object/ExceptionResponse.java
+++ rdap-renderer-api/src/main/java/mx/nic/rdap/renderer/object/ExceptionResponse.java
@@ -4,8 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExceptionResponse extends RdapResponse {
+	/**
+	 * Message that describe this error response.
+	 */
 	private List<String> errorDescription;
+
+	/**
+	 * Error code number, corresponding to the HTTP response code
+	 */
 	private String errorCode;
+
+	/**
+	 * Title of this error response
+	 */
 	private String errorTitle;
 
 	public ExceptionResponse(List<String> description, String errorCode, String errorTitle) {
@@ -23,26 +34,49 @@ public class ExceptionResponse extends RdapResponse {
 		this.errorTitle = errorTitle;
 	}
 
+	/**
+	 * @return {@link List} of {@link String}s that represents a message that
+	 *         describe this error response
+	 */
 	public List<String> getDescription() {
 		return errorDescription;
 	}
 
+	/**
+	 * @return Error code number, corresponding to the HTTP response code
+	 */
 	public String getErrorCode() {
 		return errorCode;
 	}
 
+	/**
+	 * @return Title of this error response
+	 */
 	public String getErrorTitle() {
 		return errorTitle;
 	}
 
+	/**
+	 * @param description
+	 *            {@link List} of {@link String}s that represents a message that
+	 *            describe this error response
+	 */
 	public void setDescription(List<String> description) {
 		this.errorDescription = description;
 	}
 
+	/**
+	 * @param errorCode
+	 *            Error code number, corresponding to the HTTP response code
+	 */
 	public void setErrorCode(String errorCode) {
 		this.errorCode = errorCode;
 	}
 
+	/**
+	 * @param errorTitle
+	 *            Title of this error response
+	 */
 	public void setErrorTitle(String errorTitle) {
 		this.errorTitle = errorTitle;
 	}
